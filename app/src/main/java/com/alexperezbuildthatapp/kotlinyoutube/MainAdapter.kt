f226187c3c86269b21e0618a25a6c269cf037222
Alex Perez <alex_perez25@hotmail.com>
2017-12-28 16:47:32 -0800

    RecyclerView Created + Custom View

--- app/src/main/java/com/alexperezbuildthatapp/kotlinyoutube/MainAdapter.kt
+++ app/src/main/java/com/alexperezbuildthatapp/kotlinyoutube/MainAdapter.kt
@@ -1,30 +1,41 @@
 package com.alexperezbuildthatapp.kotlinyoutube
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.video_row.view.*
 
 /**
  * Created by Alex Perez on 12/28/2017.
  */
 
-class MainAdapter:RecyclerView.Adapter<customViewHolder>(){
+class MainAdapter:RecyclerView.Adapter<CustomViewHolder>(){
 
-    //numberofitems
+    //how to initiate a list
+    val videoTitles = listOf("How to Bake", "How to Fish", "How to Be Fly");
+
+    //numberofitems in our recyclerView
     override fun getItemCount(): Int {
-        return 1
+        return videoTitles.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): customViewHolder {
-        return parent
+    //Holding our view for the application
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
+        return CustomViewHolder(cellForRow)
     }
 
-    override fun onBindViewHolder(holder: customViewHolder?, position: Int) {
-
+    //Where we bind our data to our listview to be shown
+    override fun onBindViewHolder(holder: CustomViewHolder?, position: Int) {
+        val videoTitle = videoTitles[position]
+        holder?.view?.textView_videoTitle?.text = videoTitle
     }
 
 }
 
-class customViewHolder(v: View): RecyclerView.ViewHolder(v){
+//Using this class to call a custom view to show in our application
+class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
 
 }
